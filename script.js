17e41a5ae61488a858f0479085992afd87ccbc3b
Denis Gaščić <denisgascic9@gmail.com>
2022-03-12 17:23:56 +0100

    ispisuje tekst

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const video = document.querySelector("video")
+const textElem = document.querySelector("[data-text]")
 
 async function setup() {
     const striiim = await navigator.mediaDevices.getUserMedia({ video: true })
 
 video.srcObject = striiim
+video.addEventListener("playing", async() =>{
+    const worker = Tesseract.createWorker()
+   await worker.load()
+   await worker.loadLanguage("hrv")
+   worker.initialize("hrv")
+
+   const canvas = document.createElement("canvas")
+   canvas.width = video.width
+   canvas.height = video.height
+
+   document.addEventListener("keypress", async e => {
+if (e.code !== "Space") return  
+console.log("pretražujem"); 
+canvas.getContext("2d").drawImage(video, 0,0, video.width, video.height)
+const { data: {text}} = await worker.recognize(canvas)
+textElem.textContent = text
+console.log(text);
+})
+})
 }
 setup()
